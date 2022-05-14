6b30401564d0be7243fa87c047dbc5b76db439e6
Laércio Júnior <brjuninho2@gmail.com>
2022-05-14 17:01:20 -0300

    Adicionando nova classe e modificando metódos

--- src/main/java/com/mycompany/heranca/consultor/Consultor.java
+++ src/main/java/com/mycompany/heranca/consultor/Consultor.java
@@ -1,4 +1,22 @@
 package com.mycompany.heranca.consultor;
 
 public class Consultor {
+    public String cnpj;
+    public String formacaoPrincipal;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getFormacaoPrincipal() {
+        return formacaoPrincipal;
+    }
+
+    public void setFormacaoPrincipal(String formacaoPrincipal) {
+        this.formacaoPrincipal = formacaoPrincipal;
+    }
 }
---
+++ src/main/java/com/mycompany/heranca/consultor/ConsultorFinanceiro.java
@@ -0,0 +1,43 @@
+package com.mycompany.heranca.consultor;
+
+public class ConsultorFinanceiro extends Consultor{
+
+    private boolean credenciadoAnbima;
+
+    public void consultarDividas(){
+        System.out.println("Atualmente não existem dívidas!");
+        System.out.println("");
+    }
+
+    public void recomendarInvestimento(){
+        System.out.println("Carteira de investimentos e ativos está no limite! Não necessita de rebalanceamento.");
+        System.out.println("");
+    }
+
+    public void analisarFluxoDeCaixa(){
+        System.out.println("Analisando fluxo de caixa...");
+        System.out.println("Aguarde...");
+        System.out.println("Fluxo de caixa analisado!");
+        System.out.println("");
+    }
+
+    @Override
+    public String getCnpj() {
+        return super.getCnpj();
+    }
+
+    @Override
+    public void setCnpj(String CNPJ) {
+        super.setCnpj(cnpj);
+    }
+
+    @Override
+    public String getFormacaoPrincipal() {
+        return super.getFormacaoPrincipal();
+    }
+
+    @Override
+    public void setFormacaoPrincipal(String formacaoPrincipal) {
+        super.setFormacaoPrincipal(formacaoPrincipal);
+    }
+}
