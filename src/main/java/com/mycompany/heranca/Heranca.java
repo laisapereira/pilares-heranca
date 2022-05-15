daea242225bfad86e95bdedb9804abf531d223df
Laércio Júnior <brjuninho2@gmail.com>
2022-05-15 03:32:48 -0300

    Correção de bug

--- src/main/java/com/mycompany/heranca/Heranca.java
+++ src/main/java/com/mycompany/heranca/Heranca.java
@@ -15,11 +15,11 @@ public class Heranca {
         System.out.println("Testando atendentes....");
 
         System.out.println("-- Gerente --");
-        Gerente gerente = new Gerente("Malaquias", "SÃªnior", "Noturno", "Pediatria", 40 );
+        Gerente gerente = new Gerente("Malaquias", "Sênior", "Noturno", "Pediatria", 40 );
         gerente.rotacionarFuncionarios();
 
         System.out.println("-- Supervisor --");
-        Supervisor supervisor = new Supervisor(new String[]{"EmergÃªncia", "Raio X"}, "CoordenaÃ§Ã£o", 48);
+        Supervisor supervisor = new Supervisor(new String[]{"Emergência", "Raio X"}, "Coordenação", 48);
         supervisor.aceitarRelatorio();
 
         //Consultores
@@ -27,31 +27,31 @@ public class Heranca {
         System.out.println("Testando consultores....");
 
         System.out.println("-- Consultor De Vendas --");
-        ConsultorDeVendas consultorDeVendas = new ConsultorDeVendas("01.131.500/0001-25","AdministraÃ§Ã£o", 53, "Plano de saÃºde para crianÃ§as");
+        ConsultorDeVendas consultorDeVendas = new ConsultorDeVendas("01.131.500/0001-25","Administração", 53, "Plano de saúde para crianças");
         consultorDeVendas.venderProduto();
 
         System.out.println("-- Consultor Financeiro --");
-        ConsultorFinanceiro consultorFinanceiro = new ConsultorFinanceiro("79.304.886/0001-17", "CiÃªncias ContÃ¡beis",true);
+        ConsultorFinanceiro consultorFinanceiro = new ConsultorFinanceiro("79.304.886/0001-17", "Ciências Contábeis",true);
         consultorFinanceiro.recomendarInvestimento();
 
         System.out.println("-- Consultor Juridico --");
         ConsultorJuridico consultorJuridico = new ConsultorJuridico("99.291.513/0001-66","Direito","5500", "Trabalhista");
         consultorJuridico.consultarProcesso();
 
-        //MÃ©dicos
+        //Médicos
         System.out.println("");
-        System.out.println("Testando mÃ©dicos....");
+        System.out.println("Testando médicos....");
 
         System.out.println("-- Diretor --");
-        Diretor medicoDiretor = new Diretor("Principal", "9999", "ClÃ­nico Geral");
+        Diretor medicoDiretor = new Diretor("Principal", "9999", "Clínico Geral");
         medicoDiretor.mudarTurnos();
 
         System.out.println("-- Residente --");
-        Residente medicoResidente = new Residente(true, new String[]{"TerÃ§a", "Sexta"}, "JoÃ£o", "8855", "Pediatra" );
+        Residente medicoResidente = new Residente(true, new String[]{"Terça", "Sexta"}, "João", "8855", "Pediatra" );
         medicoResidente.substituirMedico();
 
         System.out.println("-- Interno --");
-        Interno medicoInterno = new Interno("ManhÃ£", "Medicina", "JoÃ£o", true, "4040", "Ginecologista");
+        Interno medicoInterno = new Interno("Manhã", "Medicina", "João", true, "4040", "Ginecologista");
         medicoInterno.reportarResponsavel();
     }
 }
